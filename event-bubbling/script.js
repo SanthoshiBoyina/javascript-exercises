170a3032beaa4f12356ae2ce98aff5d14e155ead
Santhoshi Boyina <boyinasanthoshi12@gmail.com>
2025-05-29 17:28:57 +0530

    event bubbling
    Signed-off-by: Santhoshi Boyina <boyinasanthoshi12@gmail.com>

---
+++ event-bubbling/script.js
@@ -0,0 +1,7 @@
+document.getElementById("parent").addEventListener("click", () => {
+  console.log("inside parent");
+});
+
+document.getElementById("child").addEventListener("click", () => {
+  console.log("inside child");
+});
